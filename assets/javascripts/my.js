c0e25579f2e6c0aa98e14e8723242bba19326e4b
yuh <yuhwhu@foxmail.com>
2023-10-02 01:14:49 +0800

    update

---
+++ assets/javascripts/my.js
@@ -0,0 +1,8 @@
+// // 使用JavaScript动态调整文本框的高度以匹配图片的高度
+// window.addEventListener("load", function () {
+//     var image = document.getElementById("myImage");
+//     var text = document.getElementById("myText");
+    
+//     // 设置文本框的高度等于图片的高度
+//     text.style.height = image.clientHeight + "px";
+// });
